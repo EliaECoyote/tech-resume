98cc7f9e7f332e2432a591787585fe74e05b5c80
elia_camposilvan <elia.camposilvan@rawfish.it>
2020-01-07 01:02:30 +0100

    web: jest logs colors enabled in container

--- web/scripts/test.sh
+++ web/scripts/test.sh
@@ -11,9 +11,9 @@ done
 
 case "$JEST_MODE" in
 watch)
-  yarn concurrently --names bsb,jest --prefix-colors blue.bold,yellow.bold --kill-others --kill-others-on-fail "yarn bsb -w" "NODE_ENV=test yarn jest --watchAll"
+  yarn concurrently --names bsb,jest --prefix-colors blue.bold,yellow.bold --kill-others --kill-others-on-fail "yarn bsb -w" "NODE_ENV=test yarn jest --watchAll --colors"
   ;;
 *)
-  NODE_ENV=test yarn jest
+  NODE_ENV=test yarn jest --colors
   ;;
 esac
