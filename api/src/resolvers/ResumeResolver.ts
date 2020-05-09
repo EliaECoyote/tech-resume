93f0a4c12eac1dd5510328aa63d4c2e38177a99d
elia_camposilvan <elia.camposilvan@rawfish.it>
2020-05-09 22:30:55 +0200

    basicc addResume mutation added

--- api/src/resolvers/ResumeResolver.ts
+++ api/src/resolvers/ResumeResolver.ts
@@ -1,8 +1,23 @@
-import { Resolver, Query } from "type-graphql"
+import { Resolver, Query, Mutation, Arg, InputType, Field } from "type-graphql"
 import { Resume } from "@entities/Resume"
+
+@InputType()
+class ResumeInput {
+  @Field()
+  mdTemplate: string
+
+  @Field()
+  name: string
+}
 
 @Resolver(Resume)
 export class ResumeResolver {
+  @Mutation(() => Resume)
+  async addResume(@Arg("options", () => ResumeInput) options: ResumeInput) {
+    const resume = await Resume.create(options)
+    return resume
+  }
+
   @Query(() => [Resume])
   resumeThemes() {
     return Resume.find()
