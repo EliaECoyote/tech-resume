4bc0a51a94d212078b5d26482267911775cd0d3a
elia_camposilvan <elia.camposilvan@rawfish.it>
2020-05-10 12:49:13 +0200

    Resume typegraphql definitions added

--- api/src/entities/Resume.ts
+++ api/src/entities/Resume.ts
@@ -8,24 +8,32 @@ import {
   UpdateDateColumn,
 } from "typeorm"
 import { ResumeTheme } from "./ResumeTheme"
+import { ObjectType, Field } from "type-graphql"
 
+@ObjectType()
 @Entity("resumes")
 export class Resume extends BaseEntity {
+  @Field()
   @PrimaryGeneratedColumn("uuid")
   id: string
 
+  @Field()
   @Column()
   name: string
 
+  @Field()
   @Column()
   mdTemplate: string
 
+  @Field(() => ResumeTheme)
   @ManyToOne(() => ResumeTheme)
   resumeTheme: ResumeTheme
 
+  @Field()
   @CreateDateColumn()
   createdDate: Date
 
+  @Field()
   @UpdateDateColumn()
   updatedDate: Date
 }
